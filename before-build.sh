9f684ffb130fc9576e4798b61db5c754ddb9eda3
depidsvy <spidgorny@gmail.com>
2020-01-31 21:08:44 +0100

    - make sed overwrite the file

--- before-build.sh
+++ before-build.sh
@@ -4,5 +4,6 @@ set -x    # print all executed commands to the log
 
 sed -e "s/\$API_KEY/$API_KEY/" android/app/src/main/AndroidManifestNoKey.xml > android/app/src/main/AndroidManifest.xml
 HAVERSINE_BASE=../programs/flutter/.pub-cache/hosted/pub.dartlang.org/haversine-1.0.2/lib/src/haversine_base.dart
-sed -e "s/PI/pi/" $HAVERSINE_BASE > $HAVERSINE_BASE
+sed -e "s/PI/pi/" -i $HAVERSINE_BASE
 cat $HAVERSINE_BASE
+echo Done
