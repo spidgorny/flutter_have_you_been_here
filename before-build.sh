5732b8bb093e21a32b1ccb444620984f3e9e51eb
depidsvy <spidgorny@gmail.com>
2020-01-31 20:04:00 +0100

    path haversine_base

--- before-build.sh
+++ before-build.sh
@@ -3,5 +3,5 @@ set -e    # exit on first failed command
 set -x    # print all executed commands to the log
 
 sed -e "s/\$API_KEY/$API_KEY/" android/app/src/main/AndroidManifestNoKey.xml > android/app/src/main/AndroidManifest.xml
-HAVERSINE_BASE = ../programs/flutter/.pub-cache/hosted/pub.dartlang.org/haversine-1.0.2/lib/src/haversine_base.dart
+HAVERSINE_BASE=../programs/flutter/.pub-cache/hosted/pub.dartlang.org/haversine-1.0.2/lib/src/haversine_base.dart
 set -e "s/PI/pi/" HAVERSINE_BASE > HAVERSINE_BASE
