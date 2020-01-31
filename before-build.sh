c7246dc03f92fb507792182a0ecc097cd189fb51
depidsvy <spidgorny@gmail.com>
2020-01-31 21:16:09 +0100

    - move -i before

--- before-build.sh
+++ before-build.sh
@@ -4,6 +4,6 @@ set -x    # print all executed commands to the log
 
 sed -e "s/\$API_KEY/$API_KEY/" android/app/src/main/AndroidManifestNoKey.xml > android/app/src/main/AndroidManifest.xml
 HAVERSINE_BASE=../programs/flutter/.pub-cache/hosted/pub.dartlang.org/haversine-1.0.2/lib/src/haversine_base.dart
-sed -e "s/PI/pi/" -i $HAVERSINE_BASE
+sed -i -e "s/PI/pi/" $HAVERSINE_BASE
 cat $HAVERSINE_BASE
 echo Done
