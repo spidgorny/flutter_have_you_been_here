45e05ed278ebda4ddb624abeba07a85a687c27b3
depidsvy <spidgorny@gmail.com>
2020-01-31 21:02:07 +0100

    fix cat

--- before-build.sh
+++ before-build.sh
@@ -5,4 +5,4 @@ set -x    # print all executed commands to the log
 sed -e "s/\$API_KEY/$API_KEY/" android/app/src/main/AndroidManifestNoKey.xml > android/app/src/main/AndroidManifest.xml
 HAVERSINE_BASE=../programs/flutter/.pub-cache/hosted/pub.dartlang.org/haversine-1.0.2/lib/src/haversine_base.dart
 sed -e "s/PI/pi/" $HAVERSINE_BASE > $HAVERSINE_BASE
-cat HAVERSINE_BASE
+cat $HAVERSINE_BASE
